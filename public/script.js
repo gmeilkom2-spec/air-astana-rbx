27a090e5829c9c694a2df15618aa938dfbbaad59
gmeilkom2-spec <gmeilkom2@gmail.com>
2025-12-16 16:27:58 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,43 @@
+const API = "http://localhost:3000/api";
 
+
+async function register() {
+const email = document.getElementById('regEmail').value;
+const password = document.getElementById('regPassword').value;
+
+
+const res = await fetch(API + '/register', {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({ email, password })
+});
+
+
+if (res.ok) {
+alert('Регистрация успешна');
+window.location.href = 'login.html';
+} else {
+alert('Ошибка регистрации');
+}
+}
+
+
+async function login() {
+const email = document.getElementById('loginEmail').value;
+const password = document.getElementById('loginPassword').value;
+
+
+const res = await fetch(API + '/login', {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({ email, password })
+});
+
+
+if (res.ok) {
+alert('Вход выполнен');
+window.location.href = 'index.html';
+} else {
+alert('Неверные данные');
+}
+}
